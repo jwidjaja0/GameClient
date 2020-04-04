037521f697e0bfb33e69d42b19dc0ac049ee2b97
Wadser12346 <lam.ho155@gmail.com>
2020-04-03 19:33:05 -0700

    Added more implementation to fxml and controller for the splash screen

--- src/SplashScreen/SplashController.java
+++ src/SplashScreen/SplashController.java
@@ -1,5 +1,31 @@
 package SplashScreen;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SplashController {
+    @FXML
+    Button login;
+
+    @FXML
+    Button register;
+
+    public void initialize(){
+        login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        register.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+    }
 
 }
