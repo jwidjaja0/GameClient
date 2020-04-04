b9d1d287a9c7c3fc4f2dc51238ec0f98290ad1cb
Wadser12346 <lam.ho155@gmail.com>
2020-04-04 15:45:54 -0700

    Copied ClientPublish from MessageBus to Publish class.

--- src/com/ExceptionHandled/Client/Publish.java
+++ src/com/ExceptionHandled/Client/Publish.java
@@ -1,4 +1,37 @@
 package com.ExceptionHandled.Client;
 
-public class Publish {
+import com.ExceptionHandled.GameMessages.Wrappers.Packet;
+
+import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+
+public class Publish implements Runnable {
+    //private ArrayList<ClientChatroom> chatrooms;
+    private BlockingQueue<Packet> incoming;
+
+    //public Publish(ArrayList<ClientChatroom> chatrooms, BlockingQueue<Packet> incoming){
+    public Publish(BlockingQueue<Packet> incoming){
+        //this.chatrooms = chatrooms;
+        this.incoming = incoming;
+        Thread self = new Thread(this);
+        self.start();
+    }
+
+
+    @Override
+    public void run() {
+        // Grabs available messages from incoming and processes it and sends to relevant object
+        try {
+            while (true) {
+                Packet packet = incoming.take();// Get the packet from the incoming queue
+                //TODO: Filterpackets here
+            }
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        finally {
+            System.out.println("ClientPublish closing.");
+        }
+    }
 }
