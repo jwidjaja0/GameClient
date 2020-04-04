8087cc287ff2e94f58f6f9f1cc5c6980f700e5c4
Wadser12346 <lam.ho155@gmail.com>
2020-04-04 15:34:53 -0700

    Copied ClientReceive from MessageBus to receive class.

--- src/com/ExceptionHandled/Client/Receive.java
+++ src/com/ExceptionHandled/Client/Receive.java
@@ -1,4 +1,38 @@
 package com.ExceptionHandled.Client;
 
-public class Receive {
+import com.ExceptionHandled.GameMessages.Wrappers.Packet;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+public class Receive implements Runnable {
+
+    //TODO: Change to ChatMessage
+    Socket connection;
+    private BlockingQueue<Packet> incoming;
+
+    public Receive(Socket connection, BlockingQueue<Packet> incoming){
+        this.connection = connection;
+        this.incoming = incoming;
+        Thread self = new Thread(this);
+        self.start();
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            ObjectInputStream fromServer = new ObjectInputStream(connection.getInputStream());
+            while(true){
+                incoming.add((Packet) fromServer.readObject());
+            }
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("ClientReceive closing.");
+        }
+    }
 }
