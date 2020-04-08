06e40a44642b087f70aa8cd91e6fcb0278e20a90
Wadser12346 <lam.ho155@gmail.com>
2020-04-07 18:16:03 -0700

    Created UI.

---
+++ src/com/ExceptionHandled/TicTacToeUI/Lobby/LobbyController.java
@@ -0,0 +1,46 @@
+package com.ExceptionHandled.TicTacToeUI.Lobby;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class LobbyController {
+    @FXML
+    ListView gamesList;
+
+    @FXML
+    Button createGameButton;
+
+    @FXML
+    Button joinGameButton;
+
+    @FXML
+    Button spectateButton;
+
+    public void initialize(){
+        createGameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        joinGameButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        spectateButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+
+            }
+        });
+    }
+
+
+}
