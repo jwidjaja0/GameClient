429625fb93ac947c280e25a81ae96c25dfd1fa9a
Wadser12346 <lam.ho155@gmail.com>
2020-04-15 19:00:09 -0700

    Added all implementation for StatsViewer. Need to test now.

--- src/com/ExceptionHandled/TicTacToeUI/ViewStats/StatsViewerController.java
+++ src/com/ExceptionHandled/TicTacToeUI/ViewStats/StatsViewerController.java
@@ -1,11 +1,18 @@
 package com.ExceptionHandled.TicTacToeUI.ViewStats;
 
+import com.ExceptionHandled.GameMessages.Interfaces.Game;
 import com.ExceptionHandled.GameMessages.Interfaces.Stats;
+import com.ExceptionHandled.GameMessages.Stats.GameHistorySummary;
 import com.ExceptionHandled.GameMessages.Stats.PlayerStatsInfo;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 public class StatsViewerController {
     @FXML
@@ -18,10 +25,26 @@ public class StatsViewerController {
     private Label losses;
 
     @FXML
-    private ListView<String> gamesList;
+    private TableView<GameHistorySummary> gamesList;
+
+    @FXML
+    private TableColumn<GameHistorySummary, String> gameID;
+
+    @FXML
+    private TableColumn<GameHistorySummary, String> player1;
+
+    @FXML
+    private TableColumn<GameHistorySummary, String> player2;
+
+    @FXML
+    private TableColumn<GameHistorySummary, String> matchResult;
 
     public void initialize(){
-        
+        //Sets the Cell Value Factory identifiers for each column
+        gameID.setCellValueFactory(new PropertyValueFactory<>("gameID"));
+        player1.setCellValueFactory(new PropertyValueFactory<>("player1"));
+        player2.setCellValueFactory(new PropertyValueFactory<>("player2"));
+        matchResult.setCellValueFactory(new PropertyValueFactory<>("matchResult"));
     }
 
 
@@ -42,7 +65,7 @@ public class StatsViewerController {
                 //Set losses
                 losses.setText(String.valueOf(info.getLossAmount()));
                 //Display game history
-
+                gamesList.getItems().addAll(info.getGameStatsInfoList());
             }
         });
     }
