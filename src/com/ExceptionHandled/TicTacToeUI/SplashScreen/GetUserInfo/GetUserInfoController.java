08ec27bcb9c24e58b2157970ca04865de3c4cf7a
Joseph Widjaja <jwidjaja0@saddleback.edu>
2020-04-20 12:22:25 -0700

    changed password from textfield to passwordField

--- src/com/ExceptionHandled/TicTacToeUI/SplashScreen/GetUserInfo/GetUserInfoController.java
+++ src/com/ExceptionHandled/TicTacToeUI/SplashScreen/GetUserInfo/GetUserInfoController.java
@@ -8,6 +8,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 import java.util.Observable;
@@ -22,7 +23,7 @@ public class GetUserInfoController extends Observable {
     TextField username;
 
     @FXML
-    TextField password;
+    PasswordField password;
 
     @FXML
     TextField firstName;
